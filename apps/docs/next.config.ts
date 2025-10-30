7f9c7cb0dc5296555397b5991193eabcbb143e8d
taihv <hotai0907998139@gmail.com>
2025-10-31 00:49:38 +0700

    Clean up: remove nested git and unify monorepo

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // build ra thư mục out/
 };
 
 export default nextConfig;
